350cc897b66607b46229b045abfdb4291ea52bfd
RadoDominique <radodominique1@gmail.com>
2022-10-07 12:07:25 +0200

    Mise à jour

---
+++ README.md
@@ -0,0 +1,21 @@
+# DOCUMENTATION MOTUS GAME
+
+## AXEL SERROR ET DOMINIQUE RADO RAKOTO 
+
+### Description
+Une application reprenant le principe du jeu de motus
+
+### Usage
+Il faut installer au prealable express avec 
+
+```
+npm install express express-session cookie-parser
+
+```
+
+Demarrer avec 
+
+```
+node script.js
+
+```
--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express();
+const sessions = require('express-session')
+const cookieParser = require("cookie-parser");
+const expiryDate = new Date(Date.now() + 60 * 60 * 1000) // 1 hour
+
 const port = process.env.PORT || 3005 ;
 const fs = require('fs');
 const date = new Date();
@@ -8,7 +12,64 @@ var os = require("os");
 const { dirname } = require('path');
 var hostname = os.hostname();
 
+// SESSION
+app.use(sessions({
+  name: 'session',
+  secret: "scrtkey",
+  // keys: ['key1', 'key2'],
+  cookie: {
+    secure: true,
+    httpOnly: true,
+    // domain: 'example.com',
+    // path: 'foo/bar',
+    expires: expiryDate
+  },
+  saveUninitialized: true,
+  resave: false
+}))
+
+// parsing the incoming data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//serving public file
 app.use(express.static('ww'));
+
+// cookie parser middleware
+app.use(cookieParser());
+
+//username and password
+const myusername = 'axel'
+const mypassword = '92i'
+
+// a variable to save a session
+var session;
+
+app.get('/login',(req,res) =>{
+  session = req.session;
+  if(session.userid){
+    res.send("Bienvenue joueur,<a href=\'/logout'>click to logout</a>");
+  }else
+  res.sendFile('ww/login.html',{root:__dirname})
+})
+
+app.post('/user',(req,res) => {
+  if(req.body.username == myusername && req.body.password == mypassword){
+    session=req.session;
+    session.userid=req.body.username;
+    console.log(req.session)
+    res.redirect('/front.html');
+}
+else{
+    res.redirect('/login.html#invalid');
+    console.log('Invalid username or password');
+}
+})
+
+app.get('/logout',(req,res) => {
+  req.session.destroy();
+  res.redirect('/login');
+});
 
 app.use((req,res,next)=>{
  // console.log(req.path)
